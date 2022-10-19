ef8d3de0b4b5c7c77eac6c5faee9fed69ab6c8bc
Hunter <WilsHunt604@gotvdsb.ca>
2022-10-19 09:20:24 -0400

    stuff

--- MyMethods.cs
+++ MyMethods.cs
@@ -8,6 +8,16 @@ namespace MyMethods
 {
     internal class MyMethods
     {
+        /// <summary>
+        /// Returns the hypotenuse of a right triangle given the sides a and b.
+        /// </summary>
+        /// <param name="a">Length of side a</param>
+        /// <param name="b">Length of side b</param>
+        /// <returns>Length of side c</returns>
+        public static double GetHypotenuse(double a, double b)
+        {
+            return Math.Sqrt((a * a) + (b * b));
+        }
 
     }
 }
