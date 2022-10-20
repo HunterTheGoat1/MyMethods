808b3c5e4bb298535065f161888f357bf5706100
Hunter <WilsHunt604@gotvdsb.ca>
2022-10-20 09:29:05 -0400

    e

--- MyMethods.cs
+++ MyMethods.cs
@@ -9,14 +9,24 @@ namespace MyMethods
     internal class MyMethods
     {
         /// <summary>
-        /// Returns the hypotenuse of a right triangle given the sides a and b.
+        /// Returns the hypotenuse of a right triangle given the length of both legs.
         /// </summary>
-        /// <param name="a">Length of side a</param>
-        /// <param name="b">Length of side b</param>
-        /// <returns>Length of side c</returns>
-        public static double GetHypotenuse(double a, double b)
+        /// <param name="legOne">Length of leg one</param>
+        /// <param name="legTwo">Length of leg two</param>
+        /// <returns>Length of the hypotenuse</returns>
+        public static double GetHypotenuse(double legOne, double legTwo)
         {
-            return Math.Sqrt((a * a) + (b * b));
+            return Math.Sqrt((legOne * legOne) + (legTwo * legTwo));
+        }
+        /// <summary>
+        /// Returns the length of the other leg given the length of the hypotenuse and other leg.
+        /// </summary>
+        /// <param name="hypot">Length of the hypotenuse</param>
+        /// <param name="otherLeg">Length of the other leg</param>
+        /// <returns>Length of the hypotenuse</returns>
+        public static double GetLeg(double hypot, double otherLeg)
+        {
+            return Math.Sqrt((hypot * hypot) - (otherLeg * otherLeg));
         }
 
     }
